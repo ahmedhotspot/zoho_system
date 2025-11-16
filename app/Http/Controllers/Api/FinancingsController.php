25ce20976dcb1d59458328e22f6f31e27ba28d04
ahmedhotspot <Ahmad.Mohammad@hotspot.sa>
2025-11-16 12:47:12 +0200

    edit by Hamada  add_new_financings

--- app/Http/Controllers/Api/FinancingsController.php
+++ app/Http/Controllers/Api/FinancingsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Companie;
+use App\Models\Financing;
 use Illuminate\Http\Request;
 
 class FinancingsController extends Controller
@@ -12,9 +13,25 @@ class FinancingsController extends Controller
     {
 
         $company = Companie::where('user_id', $request->user_id)->first();
-        if (! $company) {
-            return response('not null', 404);
+        if (!$company){
+            return response('not null',404);
         }
 
+
+        Financing::create([
+            'name'=>$request->name,
+            'phone'=>$request->phone,
+            'iqama_number'=>$request->iqama_number,
+            'application_id'=>$request->application_id,
+            'financingcompanies'=>$request->company_name,
+            'price'=>$request->amount,
+            'company_id'=>$company->id,
+        ]);
+
+
+
+        
+
+    return response('suucess',200);
     }
 }
